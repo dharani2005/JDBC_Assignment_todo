cc0f6209f7ae0e329d5da6272c2111cde281aecb
dharani2005 <58557415+dharani2005@users.noreply.github.com>
2024-05-11 23:26:44 +0200

    Implemented update and delete methods in PersonDAOImpl and Implemented few methods in TodoDAOImpl except four methods.

--- src/main/java/org/example/DAOImple/PersonDAOImpl.java
+++ src/main/java/org/example/DAOImple/PersonDAOImpl.java
@@ -12,18 +12,17 @@ public class PersonDAOImpl implements PersonDAO {
     @Override
     public Person create(Person person) {
         String query = "insert into person(first_name,last_name)values(?, ?)";
-        try(Connection connection = MySqlConnection.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(query)){
-        preparedStatement.setString(1, person.getFirstName());
-        preparedStatement.setString(2, person.getLastName());
-        int rowInserted = preparedStatement.executeUpdate();
-        if(rowInserted>0){
-            System.out.println("Successfully added the person");
-        }
-        }catch(SQLException e)
-        {
+        try (Connection connection = MySqlConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, person.getFirstName());
+            preparedStatement.setString(2, person.getLastName());
+            int rowInserted = preparedStatement.executeUpdate();
+            if (rowInserted > 0) {
+                System.out.println("Successfully added the person");
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("person detailes are not created"+e.getMessage());
+            System.out.println("person detailes are not created" + e.getMessage());
         }
         return person;
     }
@@ -32,19 +31,19 @@ public class PersonDAOImpl implements PersonDAO {
     public List<Person> findAll() {
         List<Person> personList = new ArrayList<>();
         try (Connection connection = MySqlConnection.getConnection();
-             Statement statement = connection.createStatement()){
-            try(ResultSet resultSet = statement.executeQuery("select * from person")){
-                while(resultSet.next()){
+             Statement statement = connection.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery("select * from person")) {
+                while (resultSet.next()) {
                     int id = resultSet.getInt(1);
                     String firstName = resultSet.getString(2);
                     String lastName = resultSet.getString(3);
-                    Person person = new Person(id ,firstName,lastName);
+                    Person person = new Person(id, firstName, lastName);
                     personList.add(person);
                 }
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("Can not find list of all persons"+e.getMessage());
+            System.out.println("Can not find list of all persons" + e.getMessage());
         }
         return personList;
     }
@@ -53,21 +52,21 @@ public class PersonDAOImpl implements PersonDAO {
     public Person findById(int id) {
         Person person = null;
         String query = "select * from person where person_id= ?";
-       try(Connection connection = MySqlConnection.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(query)){
-        preparedStatement.setInt(1,id);
-        try(ResultSet resultSet = preparedStatement.executeQuery()){
-            if(resultSet.next()){
-                int personId = resultSet.getInt(1);
-                String firstName = resultSet.getString(2);
-                String lastName = resultSet.getString(3);
-                 person = new Person(personId,firstName,lastName);
+        try (Connection connection = MySqlConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    int personId = resultSet.getInt(1);
+                    String firstName = resultSet.getString(2);
+                    String lastName = resultSet.getString(3);
+                    person = new Person(personId, firstName, lastName);
+                }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("person found by id" + e.getMessage());
         }
-       }catch(SQLException e){
-           e.printStackTrace();
-           System.out.println("person found by id"+e.getMessage());
-       }
         return person;
     }
 
@@ -75,35 +74,61 @@ public class PersonDAOImpl implements PersonDAO {
     public List<Person> findByName(String name) {
         List<Person> personList = new ArrayList<>();
         String query = "select * from person where first_name like ?";
-        try(Connection connection = MySqlConnection.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(query)){
-            preparedStatement.setString(1,name);
-            try(ResultSet resultSet = preparedStatement.executeQuery()) {
-                while(resultSet.next()){
+        try (Connection connection = MySqlConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, name);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
                     int id = resultSet.getInt(1);
                     String firstName = resultSet.getString(2);
                     String lastName = resultSet.getString(3);
-                    Person person = new Person(id,firstName,lastName);
+                    Person person = new Person(id, firstName, lastName);
                     personList.add(person);
                 }
 
             }
 
-        }catch(SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("Person can not be found with this name"+e.getMessage());
+            System.out.println("Person can not be found with this name" + e.getMessage());
         }
         return personList;
     }
 
     @Override
     public Person update(Person person) {
+        String query = "update person set first_name = ?,last_name = ?";
+        try(Connection connection = MySqlConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query)){
+        preparedStatement.setString(1, person.getFirstName());
+        preparedStatement.setString(2, person.getLastName());
+        int rowUpdated = preparedStatement.executeUpdate();
+        if(rowUpdated>0){
+            System.out.println("person updated successfully");
+        }
+        }catch(SQLException e)
+        {
+            e.printStackTrace();
+            System.out.println("person detailes are not updated"+e.getMessage());
+        }
         return null;
     }
 
     @Override
     public boolean deleteById(int id) {
+        String query = "delete from person where person_id = ?";
+        try(Connection connection = MySqlConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query)){
+        preparedStatement.setInt(1,id);
+      int rowDeleted = preparedStatement.executeUpdate();
+      if(rowDeleted > 0){
+          System.out.println("successfully deleted");
+          return true;
+      }
+        }catch (SQLException e){
+         e.printStackTrace();
+            System.out.println("person is not deleted"+e.getMessage());
+        }
         return false;
     }
 }
--- src/main/java/org/example/DAOImple/TodoItemsDAOImpl.java
+++ src/main/java/org/example/DAOImple/TodoItemsDAOImpl.java
@@ -1,24 +1,83 @@
 package org.example.DAOImple;
+import connection.MySqlConnection;
 import org.example.DAO.TodoItemsDAO;
 import org.example.Person;
 import org.example.Todo;
 
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TodoItemsDAOImpl implements TodoItemsDAO {
     @Override
     public Todo create(Todo todo) {
-        return null;
+        String query = "insert into todo_item(title,description,deadline,done,assignee_id)values(?, ?, ?,?, ?)";
+        try(Connection connection = MySqlConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setString(1,todo.getTitle());
+            preparedStatement.setString(2,todo.getDescription());
+            preparedStatement.setDate(3,Date.valueOf(todo.getDeadline()));
+            preparedStatement.setInt(4,todo.getAssigneeId());
+            int rowInserted = preparedStatement.executeUpdate();
+            if(rowInserted>0){
+                System.out.println("successfully added the todo item");
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Todo item in not added"+e.getMessage());
+        }
+        return todo;
     }
 
     @Override
     public List<Todo> findAll() {
-        return null;
+        List<Todo> todoList = new ArrayList<>();
+        try(Connection connection = MySqlConnection.getConnection();
+            Statement statement = connection.createStatement()){
+            try(ResultSet resultSet = statement.executeQuery("select * from todo_item")){
+                while(resultSet.next()){
+                    int todoId = resultSet.getInt(1);
+                    String title = resultSet.getString(2);
+                    String description = resultSet.getString(3);
+                    LocalDate deadline = resultSet.getDate(4).toLocalDate();
+                    boolean done = resultSet.getBoolean(5);
+                    int asigneeId = resultSet.getInt(5);
+                    Todo todo = new Todo(todoId,title,description,deadline,done,asigneeId);
+                    todoList.add(todo);
+
+                }
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Todo items list is not found"+e.getMessage());
+        }
+        return todoList;
     }
 
     @Override
     public Todo findById(int id) {
-        return null;
+        Todo todo = null;
+        try(Connection connection = MySqlConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * form todo_item where id= ?")){
+            preparedStatement.setInt(1,id);
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                int todoId = resultSet.getInt(1);
+                String title = resultSet.getString(2);
+                String description = resultSet.getString(3);
+                LocalDate deadline = resultSet.getDate(4).toLocalDate();
+                boolean done = resultSet.getBoolean(5);
+                int asigneeId = resultSet.getInt(6);
+                 todo = new Todo(todoId,title,description,deadline,done,asigneeId);
+
+            }
+        }catch (SQLException e)
+        {
+            e.printStackTrace();
+            System.out.println("todo item with this id is not found"+e.getMessage());
+        }
+        return todo;
     }
 
     @Override
@@ -43,11 +102,40 @@ public class TodoItemsDAOImpl implements TodoItemsDAO {
 
     @Override
     public Todo update(Todo todo) {
-        return null;
+        String query = "update todo_item set title = ?,description = ?,deadline = ?,done = ?";
+        try(Connection connection = MySqlConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query)){
+        preparedStatement.setString(1,todo.getTitle());
+        preparedStatement.setString(2,todo.getDescription());
+        preparedStatement.setDate(3,Date.valueOf(todo.getDeadline()));
+        preparedStatement.setBoolean(4,todo.isDone());
+        int rowUpdated = preparedStatement.executeUpdate();
+        if(rowUpdated > 0)
+        {
+            System.out.println("Todo item updated successfully");
+        }
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Todo item is not updated"+e.getMessage());
+        }
+
+        return todo;
     }
 
     @Override
     public boolean deleteById(int id) {
+        String query = "delete from todo_item where todo_id = ?";
+        try(Connection connection = MySqlConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query)){
+        int rowDeleted = preparedStatement.executeUpdate();
+        if(rowDeleted > 0){
+            System.out.println("Successfully row deleted");
+            return true;
+        }
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Todo item with thid id is not deleted"+e.getMessage());
+        }
         return false;
     }
 }
